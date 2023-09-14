9be5eca2572b759be3e42dc9ed0943efb3b7df9e
Vito Medlej <vito.medlej@gmail.com>
2023-09-14 11:53:40 +0300

    err spotted

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -43,9 +43,10 @@ try {
       const req = await fetch(`https://www.thecraftroom-lb.com/api/get-data`,{ cache: 'no-store' ,next:{revalidate:0}})
       let res = await req.json()
       console.log('res: ', res);
-      const reqImages = await fetch(`https://getpantry.cloud/apiv1/pantry/11c7944e-bdec-4962-bcad-f24fe432bca6/basket/Images`,{ next: { revalidate: 400 } })
-      let resImages : any = await  reqImages.json();
+      // const reqImages = await fetch(`https://getpantry.cloud/apiv1/pantry/11c7944e-bdec-4962-bcad-f24fe432bca6/basket/Images`,{ next: { revalidate: 400 } })
+      // let resImages : any = reqImages && await  reqImages.json();
       // let res = {data:null}
+        let resImages = null;
       return (
         <PreLoader resImages={resImages || null} data={res?.data}/>
        )
