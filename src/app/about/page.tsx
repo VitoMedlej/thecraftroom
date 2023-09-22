66b2372d9b74c0ba0282ed03fbaf76278c834345
Vito Medlej <vito.medlej@gmail.com>
2023-09-22 18:31:31 +0300

    logo

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -22,7 +22,7 @@ const Index = () => {
             ABOUT US
         </Typography>
         <Box sx={{width:'100%',height:'400px'}}>
-            <img src="https://images.pexels.com/photos/6941079/pexels-photo-6941079.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" className="img contain" />
+            <img src="https://www.thecraftroom-lb.com/icon.png" alt="" className="img contain" />
         </Box>
     
         <Box sx={{my:4}}>
