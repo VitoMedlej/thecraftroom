6a269ad53f1bf59eeaebc081ca589cec3698b25e
Vito Medlej <vito.medlej@gmail.com>
2024-05-02 21:35:39 +0300

    Page

--- src/app/track/page.tsx
+++ src/app/track/page.tsx
@@ -2,7 +2,7 @@
 import Btn from '@/Components/Btn/Btn';
 import React, { useState } from 'react'
 
-const page = () => {
+const Page = () => {
     const [orderNumber, setOrderNumber] = useState('0')
     console.log('orderNumber: ', orderNumber);
     async function fetchData() {
@@ -46,4 +46,4 @@ const page = () => {
   )
 }
 
-export default page
+export default Page
